6e3fb53888efe4fdf3390491b711ea7ab45746ff
rcrisostomo <renzo.crisostomo@tallertechnologies.pe>
2013-08-19 11:41:07 -0500

    Drawer added

--- WeatherApp/src/main/java/com/ruenzuo/weatherapp/MainActivity.java
+++ WeatherApp/src/main/java/com/ruenzuo/weatherapp/MainActivity.java
@@ -1,24 +1,118 @@
 package com.ruenzuo.weatherapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
+import android.support.v4.view.GravityCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private String drawerTitle;
+    private String currentTitle;
+    private String[] drawerOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentTitle = drawerTitle = getTitle().toString();
+        drawerOptions = getResources().getStringArray(R.array.drawer_options);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, drawerOptions));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.drawable.ic_drawer,R.string.drawer_open,R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(currentTitle);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(drawerTitle);
+            }
+        };
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
     }
 
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_OPTION_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        drawerList.setItemChecked(position, true);
+        setTitle(drawerOptions[position]);
+        drawerLayout.closeDrawer(drawerList);
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void setTitle(CharSequence title) {
+        currentTitle = title.toString();
+        getSupportActionBar().setTitle(currentTitle);
     }
-    
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    public static class PlanetFragment extends Fragment {
+
+        public static final String ARG_OPTION_NUMBER = "option_number";
+
+        public PlanetFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.content_fragment, container, false);
+            int i = getArguments().getInt(ARG_OPTION_NUMBER);
+            String option = getResources().getStringArray(R.array.drawer_options)[i];
+            getActivity().setTitle(option);
+            return rootView;
+        }
+
+    }
+
 }
